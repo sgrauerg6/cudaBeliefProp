8a9f809b421c04fcba8265805dd9956f38334e11
sgrauerg6 <sgrauerg@gmail.com>
2019-05-29 16:35:43 -0400

    Add back processing of half-sized cones image in python script

--- runCudaBeliefProp.py
+++ runCudaBeliefProp.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
 	conesImageSetHalfSize = {"RefImage" : "\"conesH/im2.ppm\"", "TestImage" : "\"conesH/im6.ppm\"", "CompGpuDispMap" : "\"conesH/computedDisparityMapTsukubaGPU.pgm\"", "CompCpuDispMap" : "\"conesH/computedDisparityMapTsukubaCPU.pgm\"", "NumDispVals" : "127", "ScaleBp" : "2.0f", "GroundTruthDisp" : "\"conesH/disp2.pgm\"", "GroundTruthDispScale" : "2.0f", "bpLevelsImageSet" : 6}
 
 	#imageSets = [tsukubaImageSet, venusImageSet, barn1ImageSet, barn2ImageSet, bullImageSet, mapImageSet, posterImageSet, sawtoothImageSet, conesImageSetQuarterSize, teddyImageSetQuarterSize, conesImageSetHalfSize, teddyImageSetHalfSize, conesImageSetHalfSizeAdjusted, teddyImageSetHalfSizeAdjusted]
-	imageSets = [tsukubaImageSet, venusImageSet, barn1ImageSet, conesImageSetQuarterSize]#, conesImageSetHalfSize]
+	imageSets = [tsukubaImageSet, venusImageSet, barn1ImageSet, conesImageSetQuarterSize, conesImageSetHalfSize]
 	#refImages = ["\"tsukuba1.pgm\"", "\"conesQuarter2.pgm\"", "\"conesHalf2.pgm\""]
 	#testImages = ["\"tsukuba2.pgm\"", "\"conesQuarter6.pgm\"", "\"conesHalf6.pgm\""]
 	#saveDispGpuOutput = ["\"computedDisparityMapTsukubaGPU.pgm\"", "\"computedDisparityConesQuarterGPU.pgm\"", "\"computedDisparityConesHalfGPU.pgm\""]
