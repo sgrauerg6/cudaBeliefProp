1da5356f7dfb335e91506ef86ae41dfd8d3ceee5
sgrauerg6 <sgrauerg@gmail.com>
2019-06-18 02:21:45 -0400

    Remove cuda_runtime.h declaration from BpStereoProcessingOptimizedCPU

--- OptimizeCPU/BpStereoProcessingOptimizedCPU.h
+++ OptimizeCPU/BpStereoProcessingOptimizedCPU.h
@@ -27,9 +27,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 #include "KernelBpStereoCPU.h"
 #include <vector>
 #include <algorithm>
-#include <cuda_runtime.h>
 #include <chrono>
-#include <cuda_fp16.h>
 
 class ProcessBpStereoProcessingOptimizedCPUHelperFuncts
 {
