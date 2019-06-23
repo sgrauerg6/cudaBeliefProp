e095aa0822497f7f6cfe98d092274186651fe43d
sgrauerg6 <sgrauerg@gmail.com>
2019-06-23 15:51:23 -0400

    Updated python scripts

--- runCPUBeliefProp.py
+++ runCPUBeliefProp.py
@@ -62,7 +62,9 @@ if __name__ == "__main__":
 	beliefPropDataTypeProcessing = ["0", "2"]
 	indexOptimizationSettings = ["1"]
 	cpuOptimizationSettings = ["0", "1"]
-	numOpenMPThreads = ["12", "24", "36", "48"]
+	numOpenMPThreads = ["2", "4", "6", "8"]
+	memoryAlignmentOptimizations = [0, 1]
+	numRuns = 0
 	for imageSet in imageSets:
 		for currNumBpLevelsAndIters in numBpLevelsAndIters:
 			for currTruncationDiscontCost in truncationDiscontCost:
@@ -74,74 +76,83 @@ if __name__ == "__main__":
 									for currentIndexOptimizationSettings in indexOptimizationSettings:
 										for cpuOptimizationSetting in cpuOptimizationSettings:
 											for currNumOpenMPThreads in numOpenMPThreads:
-												#uncomment if using multiple disparity values on single image	
-												#for currNumDispVals in numDispVals:
-												numDispLevels = imageSet["NumDispVals"]
-												currTruncationDiscontCost = float(numDispLevels) / 7.5
-												file = open("bpParametersFromPython.h", "w")
-												file.write("#ifndef BP_STEREO_FROM_PYTHON_H\n")
-												file.write("#define BP_STEREO_FROM_PYTHON_H\n")
-												file.write("#define REF_IMAGE_FROM_PYTHON %s\n" % imageSet["RefImage"])
-												file.write("#define TEST_IMAGE_FROM_PYTHON %s\n" % imageSet["TestImage"])
-												file.write("#define SAVE_DISPARITY_IMAGE_PATH_GPU_FROM_PYTHON %s\n" % imageSet["CompGpuDispMap"])
-												file.write("#define SAVE_DISPARITY_IMAGE_PATH_CPU_FROM_PYTHON %s\n" % imageSet["CompCpuDispMap"])
-												file.write("#define SCALE_BP_FROM_PYTHON %s\n" % imageSet["ScaleBp"])
-												file.write("#define DEFAULT_GROUND_TRUTH_DISPARITY_FILE_FROM_PYTHON %s\n" % imageSet["GroundTruthDisp"])
-												file.write("#define DEFAULT_GROUND_TRUTH_DISPARITY_SCALE_FROM_PYTHON %s\n" % imageSet["GroundTruthDispScale"])
-												#uncomment if using multiple disparity values on single image	
-												#file.write("#define NUM_POSSIBLE_DISPARITY_VALUES_FROM_PYTHON %s\n" % currNumDispVals)
-												file.write("#define NUM_POSSIBLE_DISPARITY_VALUES_FROM_PYTHON %s\n" % imageSet["NumDispVals"])
-												file.write("#define ITER_BP_FROM_PYTHON %s\n" % currNumBpLevelsAndIters["bpIters"])
-												#file.write("#define LEVELS_BP_FROM_PYTHON %s\n" % currNumBpLevelsAndIters["bpLevels"])
-												file.write("#define LEVELS_BP_FROM_PYTHON %s\n" % imageSet["bpLevelsImageSet"])
-												file.write("#define DISC_K_BP_FROM_PYTHON %s\n" % currTruncationDiscontCost)
-												file.write("#define DATA_K_BP_FROM_PYTHON %s\n" % currTruncationDataCost)
-												file.write("#define LAMBDA_BP_FROM_PYTHON %s\n" % currDataCostWeight)
-												file.write("#define SIGMA_BP_FROM_PYTHON %s\n" % currSmoothImagesSigma)
-												file.write("#define CURRENT_DATA_TYPE_PROCESSING_FROM_PYTHON %s\n" % beliefPropDataTypeToProcess)
-												file.write("#define CPU_OPTIMIZATION_SETTING_FROM_PYTHON %s\n" % cpuOptimizationSetting);
-												
-												if (optMemory == 1):
-													file.write("#define USE_OPTIMIZED_GPU_MEMORY_MANAGEMENT_FROM_PYTHON 1\n")
-												else:
-													file.write("#define USE_OPTIMIZED_GPU_MEMORY_MANAGEMENT_FROM_PYTHON 0\n")
-									
-												file.write("#define OPTIMIZED_INDEXING_SETTING_FROM_PYTHON %s\n" % currentIndexOptimizationSettings)
-												
-												file.write("#endif")
-												file.close()
-					
-												# set the number of OpenMP threads
-												os.environ["OMP_NUM_THREADS"] = currNumOpenMPThreads
-												os.system("make clean")
-												os.system("make impDriveCPU")
-												os.system("./driverCPUBp")
-				 	
-												file = open("output.txt", "r") 
-												numLabel = 0
-												for line in file:
-													lineSplit = line.split(":")
-													if (len(lineSplit) > 0):
-														if (firstLine):
-															labelNoNewLine = lineSplit[0].replace("\n", "")
-															outputLabels.append(labelNoNewLine)
-															outputData.append([])
-														if (len(lineSplit) > 1):
-															dataNoNewLine = lineSplit[1].replace("\n", "")
-															outputData[numLabel].append(dataNoNewLine)
-														numLabel += 1			
-													print(line)
-													fileOutput.write(line)
-							
-												fileOutput.write("\n\n")
-												firstLine = False
+												for memoryAlignmentOptimization in memoryAlignmentOptimizations:
+													#uncomment if using multiple disparity values on single image	
+													#for currNumDispVals in numDispVals:
+													numRuns += 1
+													numDispLevels = imageSet["NumDispVals"]
+													currTruncationDiscontCost = float(numDispLevels) / 7.5
+													file = open("bpParametersFromPython.h", "w")
+													file.write("#ifndef BP_STEREO_FROM_PYTHON_H\n")
+													file.write("#define BP_STEREO_FROM_PYTHON_H\n")
+													file.write("#define REF_IMAGE_FROM_PYTHON %s\n" % imageSet["RefImage"])
+													file.write("#define TEST_IMAGE_FROM_PYTHON %s\n" % imageSet["TestImage"])
+													file.write("#define SAVE_DISPARITY_IMAGE_PATH_GPU_FROM_PYTHON %s\n" % imageSet["CompGpuDispMap"])
+													file.write("#define SAVE_DISPARITY_IMAGE_PATH_CPU_FROM_PYTHON %s\n" % imageSet["CompCpuDispMap"])
+													file.write("#define SCALE_BP_FROM_PYTHON %s\n" % imageSet["ScaleBp"])
+													file.write("#define DEFAULT_GROUND_TRUTH_DISPARITY_FILE_FROM_PYTHON %s\n" % imageSet["GroundTruthDisp"])
+													file.write("#define DEFAULT_GROUND_TRUTH_DISPARITY_SCALE_FROM_PYTHON %s\n" % imageSet["GroundTruthDispScale"])
+													#uncomment if using multiple disparity values on single image	
+													#file.write("#define NUM_POSSIBLE_DISPARITY_VALUES_FROM_PYTHON %s\n" % currNumDispVals)
+													file.write("#define NUM_POSSIBLE_DISPARITY_VALUES_FROM_PYTHON %s\n" % imageSet["NumDispVals"])
+													file.write("#define ITER_BP_FROM_PYTHON %s\n" % currNumBpLevelsAndIters["bpIters"])
+													#file.write("#define LEVELS_BP_FROM_PYTHON %s\n" % currNumBpLevelsAndIters["bpLevels"])
+													file.write("#define LEVELS_BP_FROM_PYTHON %s\n" % imageSet["bpLevelsImageSet"])
+													file.write("#define DISC_K_BP_FROM_PYTHON %s\n" % currTruncationDiscontCost)
+													file.write("#define DATA_K_BP_FROM_PYTHON %s\n" % currTruncationDataCost)
+													file.write("#define LAMBDA_BP_FROM_PYTHON %s\n" % currDataCostWeight)
+													file.write("#define SIGMA_BP_FROM_PYTHON %s\n" % currSmoothImagesSigma)
+													file.write("#define CURRENT_DATA_TYPE_PROCESSING_FROM_PYTHON %s\n" % beliefPropDataTypeToProcess)
+													file.write("#define CPU_OPTIMIZATION_SETTING_FROM_PYTHON %s\n" % cpuOptimizationSetting);
+													
+													if (optMemory == 1):
+														file.write("#define USE_OPTIMIZED_GPU_MEMORY_MANAGEMENT_FROM_PYTHON 1\n")
+													else:
+														file.write("#define USE_OPTIMIZED_GPU_MEMORY_MANAGEMENT_FROM_PYTHON 0\n")
+
+													if (memoryAlignmentOptimization == 1):
+														file.write("#define BYTES_ALIGN_MEMORY_FROM_PYTHON 64\n")
+														file.write("#define NUM_DATA_ALIGN_WIDTH_FROM_PYTHON 16\n");
+													else:
+														file.write("#define BYTES_ALIGN_MEMORY_FROM_PYTHON 1\n")
+														file.write("#define NUM_DATA_ALIGN_WIDTH_FROM_PYTHON 1\n");
+										
+													file.write("#define OPTIMIZED_INDEXING_SETTING_FROM_PYTHON %s\n" % currentIndexOptimizationSettings)
+													
+													file.write("#endif")
+													file.close()
+						
+													# set the number of OpenMP threads
+													os.environ["OMP_NUM_THREADS"] = currNumOpenMPThreads
+													os.system("make clean")
+													os.system("make impDriveCPU")
+													os.system("./driverCPUBp")
+					 	
+													file = open("output.txt", "r") 
+													numLabel = 0
+													for line in file:
+														lineSplit = line.split(":")
+														if (len(lineSplit) > 0):
+															if (firstLine):
+																labelNoNewLine = lineSplit[0].replace("\n", "")
+																outputLabels.append(labelNoNewLine)
+																outputData.append([])
+															if (len(lineSplit) > 1):
+																dataNoNewLine = lineSplit[1].replace("\n", "")
+																outputData[numLabel].append(dataNoNewLine)
+															numLabel += 1			
+														print(line)
+														fileOutput.write(line)
+								
+													fileOutput.write("\n\n")
+													firstLine = False
 
 	for label in outputLabels:
 		fileOutputCsv.write("%s," % label)
 	fileOutputCsv.write("\n")
 	#uncomment if using multiple disparity values on single image	
 	#for i in range(len(imageSets)*len(numDispVals)*len(numBpIters)*len(numBpLevels)*len(truncationDiscontCost)*len(truncationDataCost)*len(dataCostWeight)*len(smoothImagesSigma)):
-	for i in range(len(imageSets)*len(numBpLevelsAndIters)*len(truncationDiscontCost)*len(truncationDataCost)*len(dataCostWeight)*len(smoothImagesSigma)*len(optimizedMemory)*len(beliefPropDataTypeProcessing)*len(indexOptimizationSettings)*len(cpuOptimizationSettings)*len(numOpenMPThreads)):
+	for i in range(numRuns):
 		for data in outputData:
 			if (len(data) == 0):
 				fileOutputCsv.write(",")
--- runCudaBeliefProp.py
+++ runCudaBeliefProp.py
@@ -26,14 +26,14 @@ if __name__ == "__main__":
 	#conesImageSetHalfSizeAdjusted = {"RefImage" : "\"conesHAdjusted/im2.ppm\"", "TestImage" : "\"conesHAdjusted/im6.ppm\"", "CompGpuDispMap" : "\"conesHAdjusted/computedDisparityMapTsukubaGPU.pgm\"", "CompCpuDispMap" : "\"conesHAdjusted/computedDisparityMapTsukubaCPU.pgm\"", "NumDispVals" : "127", "ScaleBp" : "2.0f", "GroundTruthDisp" : "\"conesHAdjusted/disp2.pgm\"", "GroundTruthDispScale" : "2.0f"}
 	#teddyImageSetHalfSizeAdjusted = {"RefImage" : "\"teddyHAdjusted/im2.ppm\"", "TestImage" : "\"teddyHAdjusted/im6.ppm\"", "CompGpuDispMap" : "\"teddyHAdjusted/computedDisparityMapTsukubaGPU.pgm\"", "CompCpuDispMap" : "\"teddyHAdjusted/computedDisparityMapTsukubaCPU.pgm\"", "NumDispVals" : "127", "ScaleBp" : "2.0f", "GroundTruthDisp" : "\"teddyH/disp2.pgm\"", "GroundTruthDispScale" : "2.0f"}
 
-	tsukubaImageSet = {"RefImage" : "\"tsukuba/scene1.row3.col3.ppm\"", "TestImage" : "\"tsukuba/scene1.row3.col4.ppm\"", "CompGpuDispMap" : "\"tsukuba/computedDisparityMapTsukubaGPU.pgm\"", "CompCpuDispMap" : "\"tsukuba/computedDisparityMapTsukubaCPU.pgm\"", "NumDispVals" : "16", "ScaleBp" : "16.0f", "GroundTruthDisp" : "\"tsukuba/truedisp.row3.col3.pgm\"", "GroundTruthDispScale" : "16.0f", "bpLevelsImageSet" : 5}
-	venusImageSet = {"RefImage" : "\"venus/im2.ppm\"", "TestImage" : "\"venus/im6.ppm\"", "CompGpuDispMap" : "\"venus/computedDisparityMapTsukubaGPU.pgm\"", "CompCpuDispMap" : "\"venus/computedDisparityMapTsukubaCPU.pgm\"", "NumDispVals" : "21", "ScaleBp" : "8.0f", "GroundTruthDisp" : "\"venus/disp2.pgm\"", "GroundTruthDispScale" : "8.0f", "bpLevelsImageSet" : 5}
-	barn1ImageSet = {"RefImage" : "\"barn1/im2.ppm\"", "TestImage" : "\"barn1/im6.ppm\"", "CompGpuDispMap" : "\"barn1/computedDisparityMapTsukubaGPU.pgm\"", "CompCpuDispMap" : "\"barn1/computedDisparityMapTsukubaCPU.pgm\"", "NumDispVals" : "32", "ScaleBp" : "8.0f", "GroundTruthDisp" : "\"barn1/disp2.pgm\"", "GroundTruthDispScale" : "8.0f", "bpLevelsImageSet" : 5}
-	conesImageSetQuarterSize = {"RefImage" : "\"cones/im2.ppm\"", "TestImage" : "\"cones/im6.ppm\"", "CompGpuDispMap" : "\"cones/computedDisparityMapTsukubaGPU.pgm\"", "CompCpuDispMap" : "\"cones/computedDisparityMapTsukubaCPU.pgm\"", "NumDispVals" : "64", "ScaleBp" : "4.0f", "GroundTruthDisp" : "\"cones/disp2.pgm\"", "GroundTruthDispScale" : "4.0f", "bpLevelsImageSet" : 5}
-	conesImageSetHalfSize = {"RefImage" : "\"conesH/im2.ppm\"", "TestImage" : "\"conesH/im6.ppm\"", "CompGpuDispMap" : "\"conesH/computedDisparityMapTsukubaGPU.pgm\"", "CompCpuDispMap" : "\"conesH/computedDisparityMapTsukubaCPU.pgm\"", "NumDispVals" : "128", "ScaleBp" : "2.0f", "GroundTruthDisp" : "\"conesH/disp2.pgm\"", "GroundTruthDispScale" : "2.0f", "bpLevelsImageSet" : 5}
+	tsukubaImageSet = {"RefImage" : "\"tsukuba/scene1.row3.col3.ppm\"", "TestImage" : "\"tsukuba/scene1.row3.col4.ppm\"", "CompGpuDispMap" : "\"tsukuba/computedDisparityMapTsukubaGPU.pgm\"", "CompCpuDispMap" : "\"tsukuba/computedDisparityMapTsukubaCPU.pgm\"", "NumDispVals" : "16", "ScaleBp" : "16.0f", "GroundTruthDisp" : "\"tsukuba/truedisp.row3.col3.pgm\"", "GroundTruthDispScale" : "16.0f", "bpLevelsImageSet" : 2}
+	venusImageSet = {"RefImage" : "\"venus/im2.ppm\"", "TestImage" : "\"venus/im6.ppm\"", "CompGpuDispMap" : "\"venus/computedDisparityMapTsukubaGPU.pgm\"", "CompCpuDispMap" : "\"venus/computedDisparityMapTsukubaCPU.pgm\"", "NumDispVals" : "21", "ScaleBp" : "8.0f", "GroundTruthDisp" : "\"venus/disp2.pgm\"", "GroundTruthDispScale" : "8.0f", "bpLevelsImageSet" : 2}
+	barn1ImageSet = {"RefImage" : "\"barn1/im2.ppm\"", "TestImage" : "\"barn1/im6.ppm\"", "CompGpuDispMap" : "\"barn1/computedDisparityMapTsukubaGPU.pgm\"", "CompCpuDispMap" : "\"barn1/computedDisparityMapTsukubaCPU.pgm\"", "NumDispVals" : "32", "ScaleBp" : "8.0f", "GroundTruthDisp" : "\"barn1/disp2.pgm\"", "GroundTruthDispScale" : "8.0f", "bpLevelsImageSet" : 2}
+	conesImageSetQuarterSize = {"RefImage" : "\"cones/im2.ppm\"", "TestImage" : "\"cones/im6.ppm\"", "CompGpuDispMap" : "\"cones/computedDisparityMapTsukubaGPU.pgm\"", "CompCpuDispMap" : "\"cones/computedDisparityMapTsukubaCPU.pgm\"", "NumDispVals" : "64", "ScaleBp" : "4.0f", "GroundTruthDisp" : "\"cones/disp2.pgm\"", "GroundTruthDispScale" : "4.0f", "bpLevelsImageSet" : 2}
+	conesImageSetHalfSize = {"RefImage" : "\"conesH/im2.ppm\"", "TestImage" : "\"conesH/im6.ppm\"", "CompGpuDispMap" : "\"conesH/computedDisparityMapTsukubaGPU.pgm\"", "CompCpuDispMap" : "\"conesH/computedDisparityMapTsukubaCPU.pgm\"", "NumDispVals" : "128", "ScaleBp" : "2.0f", "GroundTruthDisp" : "\"conesH/disp2.pgm\"", "GroundTruthDispScale" : "2.0f", "bpLevelsImageSet" : 2}
 
 	#imageSets = [tsukubaImageSet, venusImageSet, barn1ImageSet, barn2ImageSet, bullImageSet, mapImageSet, posterImageSet, sawtoothImageSet, conesImageSetQuarterSize, teddyImageSetQuarterSize, conesImageSetHalfSize, teddyImageSetHalfSize, conesImageSetHalfSizeAdjusted, teddyImageSetHalfSizeAdjusted]
-	imageSets = [tsukubaImageSet, venusImageSet, barn1ImageSet, conesImageSetQuarterSize]
+	imageSets = [tsukubaImageSet, venusImageSet, barn1ImageSet, conesImageSetQuarterSize, conesImageSetHalfSize]
 	#refImages = ["\"tsukuba1.pgm\"", "\"conesQuarter2.pgm\"", "\"conesHalf2.pgm\""]
 	#testImages = ["\"tsukuba2.pgm\"", "\"conesQuarter6.pgm\"", "\"conesHalf6.pgm\""]
 	#saveDispGpuOutput = ["\"computedDisparityMapTsukubaGPU.pgm\"", "\"computedDisparityConesQuarterGPU.pgm\"", "\"computedDisparityConesHalfGPU.pgm\""]
@@ -59,9 +59,11 @@ if __name__ == "__main__":
 	outputData = []
 	firstLine = True
 	optimizedMemory = [0, 1]
-	beliefPropDataTypeProcessing = ["0"]
+	beliefPropDataTypeProcessing = ["0", "2"]
 	indexOptimizationSettings = ["1"]
 	cpuOptimizationSettings = ["1"]
+	memoryAlignmentOptimizations = [0, 1]
+	numRuns = 0
 	for imageSet in imageSets:
 		for currNumBpLevelsAndIters in numBpLevelsAndIters:
 			for currTruncationDiscontCost in truncationDiscontCost:
@@ -72,72 +74,81 @@ if __name__ == "__main__":
 								for beliefPropDataTypeToProcess in beliefPropDataTypeProcessing:
 									for currentIndexOptimizationSettings in indexOptimizationSettings:
 										for cpuOptimizationSetting in cpuOptimizationSettings:
-											#uncomment if using multiple disparity values on single image	
-											#for currNumDispVals in numDispVals:
-											numDispLevels = imageSet["NumDispVals"]
-											currTruncationDiscontCost = float(numDispLevels) / 7.5
-											file = open("bpParametersFromPython.h", "w")
-											file.write("#ifndef BP_STEREO_FROM_PYTHON_H\n")
-											file.write("#define BP_STEREO_FROM_PYTHON_H\n")
-											file.write("#define REF_IMAGE_FROM_PYTHON %s\n" % imageSet["RefImage"])
-											file.write("#define TEST_IMAGE_FROM_PYTHON %s\n" % imageSet["TestImage"])
-											file.write("#define SAVE_DISPARITY_IMAGE_PATH_GPU_FROM_PYTHON %s\n" % imageSet["CompGpuDispMap"])
-											file.write("#define SAVE_DISPARITY_IMAGE_PATH_CPU_FROM_PYTHON %s\n" % imageSet["CompCpuDispMap"])
-											file.write("#define SCALE_BP_FROM_PYTHON %s\n" % imageSet["ScaleBp"])
-											file.write("#define DEFAULT_GROUND_TRUTH_DISPARITY_FILE_FROM_PYTHON %s\n" % imageSet["GroundTruthDisp"])
-											file.write("#define DEFAULT_GROUND_TRUTH_DISPARITY_SCALE_FROM_PYTHON %s\n" % imageSet["GroundTruthDispScale"])
-											#uncomment if using multiple disparity values on single image	
-											#file.write("#define NUM_POSSIBLE_DISPARITY_VALUES_FROM_PYTHON %s\n" % currNumDispVals)
-											file.write("#define NUM_POSSIBLE_DISPARITY_VALUES_FROM_PYTHON %s\n" % imageSet["NumDispVals"])
-											file.write("#define ITER_BP_FROM_PYTHON %s\n" % currNumBpLevelsAndIters["bpIters"])
-											#file.write("#define LEVELS_BP_FROM_PYTHON %s\n" % currNumBpLevelsAndIters["bpLevels"])
-											file.write("#define LEVELS_BP_FROM_PYTHON %s\n" % imageSet["bpLevelsImageSet"])
-											file.write("#define DISC_K_BP_FROM_PYTHON %s\n" % currTruncationDiscontCost)
-											file.write("#define DATA_K_BP_FROM_PYTHON %s\n" % currTruncationDataCost)
-											file.write("#define LAMBDA_BP_FROM_PYTHON %s\n" % currDataCostWeight)
-											file.write("#define SIGMA_BP_FROM_PYTHON %s\n" % currSmoothImagesSigma)
-											file.write("#define CURRENT_DATA_TYPE_PROCESSING_FROM_PYTHON %s\n" % beliefPropDataTypeToProcess)
-											file.write("#define CPU_OPTIMIZATION_SETTING_FROM_PYTHON %s\n" % cpuOptimizationSetting);
-											
-											if (optMemory == 1):
-												file.write("#define USE_OPTIMIZED_GPU_MEMORY_MANAGEMENT_FROM_PYTHON 1\n")
-											else:
-												file.write("#define USE_OPTIMIZED_GPU_MEMORY_MANAGEMENT_FROM_PYTHON 0\n")
-								
-											file.write("#define OPTIMIZED_INDEXING_SETTING_FROM_PYTHON %s\n" % currentIndexOptimizationSettings)
-											
-											file.write("#endif")
-											file.close()
-				
-											os.system("make clean")
-											os.system("make")
-											os.system("./driverCudaBp")
-			 	
-											file = open("output.txt", "r") 
-											numLabel = 0
-											for line in file:
-												lineSplit = line.split(":")
-												if (len(lineSplit) > 0):
-													if (firstLine):
-														labelNoNewLine = lineSplit[0].replace("\n", "")
-														outputLabels.append(labelNoNewLine)
-														outputData.append([])
-													if (len(lineSplit) > 1):
-														dataNoNewLine = lineSplit[1].replace("\n", "")
-														outputData[numLabel].append(dataNoNewLine)
-													numLabel += 1			
-												print(line)
-												fileOutput.write(line)
-						
-											fileOutput.write("\n\n")
-											firstLine = False
+											for memoryAlignmentOptimization in memoryAlignmentOptimizations:
+												#uncomment if using multiple disparity values on single image	
+												#for currNumDispVals in numDispVals:
+												numRuns += 1
+												numDispLevels = imageSet["NumDispVals"]
+												currTruncationDiscontCost = float(numDispLevels) / 7.5
+												file = open("bpParametersFromPython.h", "w")
+												file.write("#ifndef BP_STEREO_FROM_PYTHON_H\n")
+												file.write("#define BP_STEREO_FROM_PYTHON_H\n")
+												file.write("#define REF_IMAGE_FROM_PYTHON %s\n" % imageSet["RefImage"])
+												file.write("#define TEST_IMAGE_FROM_PYTHON %s\n" % imageSet["TestImage"])
+												file.write("#define SAVE_DISPARITY_IMAGE_PATH_GPU_FROM_PYTHON %s\n" % imageSet["CompGpuDispMap"])
+												file.write("#define SAVE_DISPARITY_IMAGE_PATH_CPU_FROM_PYTHON %s\n" % imageSet["CompCpuDispMap"])
+												file.write("#define SCALE_BP_FROM_PYTHON %s\n" % imageSet["ScaleBp"])
+												file.write("#define DEFAULT_GROUND_TRUTH_DISPARITY_FILE_FROM_PYTHON %s\n" % imageSet["GroundTruthDisp"])
+												file.write("#define DEFAULT_GROUND_TRUTH_DISPARITY_SCALE_FROM_PYTHON %s\n" % imageSet["GroundTruthDispScale"])
+												#uncomment if using multiple disparity values on single image	
+												#file.write("#define NUM_POSSIBLE_DISPARITY_VALUES_FROM_PYTHON %s\n" % currNumDispVals)
+												file.write("#define NUM_POSSIBLE_DISPARITY_VALUES_FROM_PYTHON %s\n" % imageSet["NumDispVals"])
+												file.write("#define ITER_BP_FROM_PYTHON %s\n" % currNumBpLevelsAndIters["bpIters"])
+												#file.write("#define LEVELS_BP_FROM_PYTHON %s\n" % currNumBpLevelsAndIters["bpLevels"])
+												file.write("#define LEVELS_BP_FROM_PYTHON %s\n" % imageSet["bpLevelsImageSet"])
+												file.write("#define DISC_K_BP_FROM_PYTHON %s\n" % currTruncationDiscontCost)
+												file.write("#define DATA_K_BP_FROM_PYTHON %s\n" % currTruncationDataCost)
+												file.write("#define LAMBDA_BP_FROM_PYTHON %s\n" % currDataCostWeight)
+												file.write("#define SIGMA_BP_FROM_PYTHON %s\n" % currSmoothImagesSigma)
+												file.write("#define CURRENT_DATA_TYPE_PROCESSING_FROM_PYTHON %s\n" % beliefPropDataTypeToProcess)
+												file.write("#define CPU_OPTIMIZATION_SETTING_FROM_PYTHON %s\n" % cpuOptimizationSetting);
+
+												if (memoryAlignmentOptimization == 1):
+													file.write("#define BYTES_ALIGN_MEMORY_FROM_PYTHON 64\n")
+													file.write("#define NUM_DATA_ALIGN_WIDTH_FROM_PYTHON 16\n");
+												else:
+													file.write("#define BYTES_ALIGN_MEMORY_FROM_PYTHON 1\n")
+													file.write("#define NUM_DATA_ALIGN_WIDTH_FROM_PYTHON 1\n");
+												
+												if (optMemory == 1):
+													file.write("#define USE_OPTIMIZED_GPU_MEMORY_MANAGEMENT_FROM_PYTHON 1\n")
+												else:
+													file.write("#define USE_OPTIMIZED_GPU_MEMORY_MANAGEMENT_FROM_PYTHON 0\n")
+									
+												file.write("#define OPTIMIZED_INDEXING_SETTING_FROM_PYTHON %s\n" % currentIndexOptimizationSettings)
+												
+												file.write("#endif")
+												file.close()
+					
+												os.system("make clean")
+												os.system("make")
+												os.system("./driverCudaBp")
+				 	
+												file = open("output.txt", "r") 
+												numLabel = 0
+												for line in file:
+													lineSplit = line.split(":")
+													if (len(lineSplit) > 0):
+														if (firstLine):
+															labelNoNewLine = lineSplit[0].replace("\n", "")
+															outputLabels.append(labelNoNewLine)
+															outputData.append([])
+														if (len(lineSplit) > 1):
+															dataNoNewLine = lineSplit[1].replace("\n", "")
+															outputData[numLabel].append(dataNoNewLine)
+														numLabel += 1			
+													print(line)
+													fileOutput.write(line)
+							
+												fileOutput.write("\n\n")
+												firstLine = False
 
 	for label in outputLabels:
 		fileOutputCsv.write("%s," % label)
 	fileOutputCsv.write("\n")
 	#uncomment if using multiple disparity values on single image	
 	#for i in range(len(imageSets)*len(numDispVals)*len(numBpIters)*len(numBpLevels)*len(truncationDiscontCost)*len(truncationDataCost)*len(dataCostWeight)*len(smoothImagesSigma)):
-	for i in range(len(imageSets)*len(numBpLevelsAndIters)*len(truncationDiscontCost)*len(truncationDataCost)*len(dataCostWeight)*len(smoothImagesSigma)*len(optimizedMemory)*len(beliefPropDataTypeProcessing)*len(indexOptimizationSettings)*len(cpuOptimizationSettings)):
+	for i in range(numRuns):
 		for data in outputData:
 			if (len(data) == 0):
 				fileOutputCsv.write(",")
