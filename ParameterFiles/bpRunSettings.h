05aa3aa8502f3be1babe9b12f55d9601a218cf57
scott gg <sgrauerg@gmail.com>
2019-09-22 00:22:53 -0400

    Add bpRunSettings.h and bpStructsAndEnums.h files

---
+++ ParameterFiles/bpRunSettings.h
@@ -0,0 +1,82 @@
+/*
+ * bpRunSettings.h
+ *
+ *  Created on: Sep 21, 2019
+ *      Author: scott
+ */
+
+#ifndef BPRUNSETTINGS_H_
+#define BPRUNSETTINGS_H_
+
+#include "bpStereoParameters.h"
+#include "bpParametersFromPython.h"
+
+#define DATA_TYPE_PROCESSING_FLOAT 0
+#define DATA_TYPE_PROCESSING_DOUBLE 1
+#define DATA_TYPE_PROCESSING_HALF 2
+//not currently supporting half2 data type
+#define DATA_TYPE_PROCESSING_HALF_TWO 3
+
+#define USE_DEFAULT 0
+#define USE_AVX_256 1
+#define USE_AVX_512 2
+#define USE_NEON 3
+
+//If image set parameters from python, then use optimization settings set in current iteration in python script
+//These settings are written to file bpParametersFromPython.h as part of the python script
+#if (IMAGE_SET_TO_PROCESS == IMAGE_SET_PARAMETERS_FROM_PYTHON)
+#define CURRENT_DATA_TYPE_PROCESSING CURRENT_DATA_TYPE_PROCESSING_FROM_PYTHON
+#define OPTIMIZED_INDEXING_SETTING OPTIMIZED_INDEXING_SETTING_FROM_PYTHON
+#define CPU_OPTIMIZATION_SETTING CPU_OPTIMIZATION_SETTING_FROM_PYTHON
+#else
+//by default, 32-bit float data is used with optimized GPU memory management and optimized indexing
+//See http://scottgg.net/OptimizingGlobalStereoMatchingOnNVIDIAGPUs.pdf for more info on these optimizations (note that the optimized indexing was present in the initial implementation)
+//Can remove optimized GPU memory management (making the processing more similar to the initial work) by commenting out the "#define USE_OPTIMIZED_GPU_MEMORY_MANAGEMENT" line
+//May be able to speed up processing by switching to using 16-bit half data by setting CURRENT_DATA_TYPE_PROCESSING to DATA_TYPE_PROCESSING_HALF
+//Optimized indexing can be turned off by changing the OPTIMIZED_INDEXING_SETTING value to 0 (not recommended; this slows down processing)
+#define CURRENT_DATA_TYPE_PROCESSING DATA_TYPE_PROCESSING_FLOAT
+#define OPTIMIZED_INDEXING_SETTING 1
+#define USE_OPTIMIZED_GPU_MEMORY_MANAGEMENT
+#define CPU_OPTIMIZATION_SETTING USE_DEFAULT
+#endif //(IMAGE_SET_TO_PROCESS == IMAGE_SET_PARAMETERS_FROM_PYTHON)
+
+//#define COMPILING_FOR_ARM
+#ifdef COMPILING_FOR_ARM
+#include <arm_neon.h> //needed for float16_t type
+#endif
+
+//remove (or don't use) capability for half precision if using GPU with compute capability under 5.3
+#if CURRENT_DATA_TYPE_PROCESSING == DATA_TYPE_PROCESSING_FLOAT
+typedef float beliefPropProcessingDataType;
+#define BELIEF_PROP_PROCESSING_DATA_TYPE_STRING "FLOAT"
+#elif CURRENT_DATA_TYPE_PROCESSING == DATA_TYPE_PROCESSING_DOUBLE
+typedef double beliefPropProcessingDataType;
+#define BELIEF_PROP_PROCESSING_DATA_TYPE_STRING "DOUBLE"
+#elif CURRENT_DATA_TYPE_PROCESSING == DATA_TYPE_PROCESSING_HALF
+#ifdef COMPILING_FOR_ARM
+typedef float16_t beliefPropProcessingDataType;
+#else
+typedef short beliefPropProcessingDataType;
+#endif
+#define BELIEF_PROP_PROCESSING_DATA_TYPE_STRING "HALF"
+//not currently supporting half2 data type
+/*#elif CURRENT_DATA_TYPE_PROCESSING == DATA_TYPE_PROCESSING_HALF_TWO
+typedef short beliefPropProcessingDataType;*/
+//#define BELIEF_PROP_PROCESSING_DATA_TYPE_STRING "HALF2"
+#endif
+
+#if (IMAGE_SET_TO_PROCESS == IMAGE_SET_PARAMETERS_FROM_PYTHON)
+#define BYTES_ALIGN_MEMORY BYTES_ALIGN_MEMORY_FROM_PYTHON
+#define NUM_DATA_ALIGN_WIDTH NUM_DATA_ALIGN_WIDTH_FROM_PYTHON
+#else
+#if CPU_OPTIMIZATION_SETTING == USE_AVX_512
+//avx512 requires data to be aligned on 64 bytes
+#define BYTES_ALIGN_MEMORY 64
+#define NUM_DATA_ALIGN_WIDTH 16
+#else
+#define BYTES_ALIGN_MEMORY 32
+#define NUM_DATA_ALIGN_WIDTH 8
+#endif
+#endif //CPU_OPTIMIZATION_SETTING == USE_AVX_512
+
+#endif /* BPRUNSETTINGS_H_ */
---
+++ ParameterFiles/bpStructsAndEnums.h
@@ -0,0 +1,49 @@
+/*
+ * bpStructsAndEnums.h
+ *
+ *  Created on: Sep 22, 2019
+ *      Author: scott
+ */
+
+#ifndef BPSTRUCTSANDENUMS_H_
+#define BPSTRUCTSANDENUMS_H_
+
+#include "bpStereoParameters.h"
+
+//structure to store the settings for the number of levels and iterations
+struct BPsettings
+{
+	int numLevels;
+	int numIterations;
+
+	float smoothingSigma;
+	float lambda_bp;
+	float data_k_bp;
+	float disc_k_bp;
+
+	//default constructor setting BPsettings
+	//to default values
+	BPsettings()
+	{
+		smoothingSigma = bp_params::SIGMA_BP;
+		numLevels = bp_params::LEVELS_BP;
+		numIterations = bp_params::ITER_BP;
+		lambda_bp = bp_params::LAMBDA_BP;
+		data_k_bp = bp_params::DATA_K_BP;
+		disc_k_bp = bp_params::DISC_K_BP;
+	}
+};
+
+//structure to store the properties of the current level
+struct levelProperties
+{
+	int widthLevel;
+	int widthCheckerboardLevel;
+	int paddedWidthCheckerboardLevel;
+	int heightLevel;
+};
+
+//used to define the two checkerboard "parts" that the image is divided into
+enum Checkerboard_Parts {CHECKERBOARD_PART_1, CHECKERBOARD_PART_2 };
+
+#endif /* BPSTRUCTSANDENUMS_H_ */
