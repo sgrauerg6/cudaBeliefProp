8ac6862a9cf6ff21586c31259e1252b75c92804b
Scott GG <sgrauerg@gmail.com>
2019-10-01 17:51:04 -0400

    Origanization/coding improvements and fixes for running BP with DLLs

--- WindowsVSRunBPWithDLLs/RunBPWithDLLs/RunBPWithDLLs/RunBpWithDLLs.cpp
+++ WindowsVSRunBPWithDLLs/RunBPWithDLLs/RunBPWithDLLs/RunBpWithDLLs.cpp
@@ -8,78 +8,30 @@
 #include <windows.h>
 #include <iostream>
 
-//needed for the current BP parameters for the costs and also the CUDA parameters such as thread block size
-#include "ParameterFiles/bpStereoCudaParameters.h"
-#include "BpAndSmoothProcessing/RunBpStereoSet.h"
-#include "FileProcessing/BpFileHandling.h"
-#include "OutputEvaluation/DisparityMap.h"
+//needed for consts and functions for running bp using DLLs
+#include "RunBpWithDLLsHelpers.h"
 
 //needed to run the implementation a stereo set using CUDA
 #include "MainDriverFiles/RunAndEvaluateBpResults.h"
 
-//typedef RunBpStereoSet<float>* (__cdecl *RunBpStereoSet_factory)();
-using RunBpStereoSetFloat_factory = RunBpStereoSet<float>* (__cdecl*)();
-using RunBpStereoSetDouble_factory = RunBpStereoSet<double>* (__cdecl*)();
-using RunBpStereoSetShort_factory = RunBpStereoSet<short>* (__cdecl*)();
-
 int main(int argc, char** argv)
 {
-	// Load the DLL
-	HINSTANCE dll_handle = ::LoadLibrary(TEXT("CUDABeliefPropDLL.dll"));
-	if (!dll_handle) {
-		std::cout << "Unable to load DLL!\n";
-		return 1;
-	}
-
-	// Get the function from the DLL
-	RunBpStereoSetFloat_factory factory_funcCUDA = reinterpret_cast<RunBpStereoSetFloat_factory>(
-		::GetProcAddress(dll_handle, "createRunBpStereoSetOnGPUWithCUDAFloat"));
-	if (!factory_funcCUDA) {
-		std::cout << "Unable to load createRunBpStereoSetOnGPUWithCUDAFloat from DLL!\n";
-		::FreeLibrary(dll_handle);
-		return 1;
-	}
-
-	// Load the DLL
-	dll_handle = ::LoadLibrary(TEXT("SingleThreadCPUBeliefPropDLL.dll"));
-	if (!dll_handle) {
-		std::cout << "Unable to load DLL!\n";
-		return 1;
-	}
-
-	// Get the function from the DLL
-	RunBpStereoSetFloat_factory factory_funcSingleThreadCPU = reinterpret_cast<RunBpStereoSetFloat_factory>(
-		::GetProcAddress(dll_handle, "createRunBpStereoCPUSingleThreadFloat"));
-	if (!factory_funcSingleThreadCPU) {
-		std::cout << "Unable to load createRunBpStereoCPUSingleThreadFloat from DLL!\n";
-		::FreeLibrary(dll_handle);
-		return 1;
-	}
-
-	// Load the DLL
-	dll_handle = ::LoadLibrary(TEXT("OptimizedCPUBeliefPropDLL.dll"));
-	if (!dll_handle) {
-		std::cout << "Unable to load DLL!\n";
-		return 1;
-	}
-
-	// Get the function from the DLL
-	RunBpStereoSetFloat_factory factory_funcOptimizedCPU = reinterpret_cast<RunBpStereoSetFloat_factory>(
-		::GetProcAddress(dll_handle, "createRunBpStereoOptimizedCPUFloat"));
-	if (!factory_funcOptimizedCPU) {
-		std::cout << "Unable to load createRunBpStereoOptimizedCPUFloat from DLL!\n";
-		::FreeLibrary(dll_handle);
-		return 1;
-	}
+	//get mapping of device config to factory function to retrieve run stereo set object for device config
+	auto runBpFactoryFuncts = RunBpWithDLLsHelpers::getRunBpFactoryFuncts();
 
 	//std::ofstream resultsStream("output.txt", std::ofstream::out);
 	std::ostream resultsStream(std::cout.rdbuf());
 	RunAndEvaluateBpResults::printParameters(resultsStream);
 
-	std::array<std::unique_ptr<RunBpStereoSet<float>>, 2> bpProcessingImps = {
-				std::unique_ptr<RunBpStereoSet<float>>(factory_funcSingleThreadCPU()),
-				std::unique_ptr<RunBpStereoSet<float>>(factory_funcCUDA())
-	};
-
+	//run single thread CPU and CUDA implementations and compare
+	std::array<std::unique_ptr<RunBpStereoSet<beliefPropProcessingDataType>>, 2> bpProcessingImps = {
+				std::unique_ptr<RunBpStereoSet<beliefPropProcessingDataType>>(runBpFactoryFuncts[run_bp_dlls::device_run::SINGLE_THREAD_CPU]()),
+				std::unique_ptr<RunBpStereoSet<beliefPropProcessingDataType>>(runBpFactoryFuncts[run_bp_dlls::device_run::CUDA]())};
 	RunAndEvaluateBpResults::runStereoTwoImpsAndCompare(resultsStream, bpProcessingImps);
+
+	//run single thread CPU and optimized implementations and compare
+	std::array<std::unique_ptr<RunBpStereoSet<beliefPropProcessingDataType>>, 2> bpProcessingImps2 = {
+				std::unique_ptr<RunBpStereoSet<beliefPropProcessingDataType>>(runBpFactoryFuncts[run_bp_dlls::device_run::SINGLE_THREAD_CPU]()),
+				std::unique_ptr<RunBpStereoSet<beliefPropProcessingDataType>>(runBpFactoryFuncts[run_bp_dlls::device_run::OPTIMIZED_CPU]())};
+	RunAndEvaluateBpResults::runStereoTwoImpsAndCompare(resultsStream, bpProcessingImps2);
 }
---
+++ WindowsVSRunBPWithDLLs/RunBPWithDLLs/RunBPWithDLLs/RunBpWithDLLsHelpers.h
@@ -0,0 +1,97 @@
+#pragma once
+
+#define NOMINMAX
+#include <memory>
+#include <fstream>
+#include <cuda_runtime.h>
+#include <windows.h>
+#include <iostream>
+#include <map>
+
+//Needed for beliefPropProcessingDataType
+#include "ParameterFiles/bpRunSettings.h"
+#include "BpAndSmoothProcessing/RunBpStereoSet.h"
+
+//typedef RunBpStereoSet<float>* (__cdecl *RunBpStereoSet_factory)();
+using RunBpStereoSet_factory = RunBpStereoSet<beliefPropProcessingDataType>* (__cdecl*)();
+
+namespace run_bp_dlls
+{
+	enum class device_run { SINGLE_THREAD_CPU, OPTIMIZED_CPU, CUDA };
+	const std::map< device_run, std::string> DLL_FILE_NAMES = { {device_run::SINGLE_THREAD_CPU, "SingleThreadCPUBeliefPropDLL.dll"},
+																{device_run::OPTIMIZED_CPU, "OptimizedCPUBeliefPropDLL.dll"},
+																{device_run::CUDA, "CUDABeliefPropDLL.dll"} };
+}
+
+class RunBpWithDLLsHelpers
+{
+public:
+	static std::wstring s2ws(const std::string& s)
+	{
+		int len;
+		int slength = (int)s.length() + 1;
+		len = MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, 0, 0);
+		wchar_t* buf = new wchar_t[len];
+		MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, buf, len);
+		std::wstring r(buf);
+		delete[] buf;
+		return r;
+	}
+
+	//retrieve name of factory function to get run stereo set class object to run stereo set for each possible device config
+	//templated with data type to process since function name is different for each data type (currently support float, double, 
+	//and short)
+	template <typename T>
+	static const std::map<run_bp_dlls::device_run, std::string> getRunStereoSetMethodNames()
+	{
+		return std::map< run_bp_dlls::device_run, std::string> { {device_run::SINGLE_THREAD_CPU, "createRunBpStereoCPUSingleThreadFloat"},
+		{ device_run::OPTIMIZED_CPU, "createRunBpStereoOptimizedCPUFloat" },
+		{ device_run::CUDA, "createRunBpStereoSetOnGPUWithCUDAFloat" } };
+	}
+
+	template <>
+	static const std::map<run_bp_dlls::device_run, std::string> getRunStereoSetMethodNames<double>()
+	{
+		return std::map< run_bp_dlls::device_run, std::string> { {run_bp_dlls::device_run::SINGLE_THREAD_CPU, "createRunBpStereoCPUSingleThreadDouble"},
+		{ run_bp_dlls::device_run::OPTIMIZED_CPU, "createRunBpStereoOptimizedCPUDouble" },
+		{ run_bp_dlls::device_run::CUDA, "createRunBpStereoSetOnGPUWithCUDADouble" } };
+	}
+
+	template <>
+	static const std::map<run_bp_dlls::device_run, std::string> getRunStereoSetMethodNames<short>()
+	{
+		return std::map< run_bp_dlls::device_run, std::string> { {run_bp_dlls::device_run::SINGLE_THREAD_CPU, "createRunBpStereoCPUSingleThreadShort"},
+		{ run_bp_dlls::device_run::OPTIMIZED_CPU, "createRunBpStereoOptimizedCPUShort" },
+		{ run_bp_dlls::device_run::CUDA, "createRunBpStereoSetOnGPUWithCUDAShort" } };
+	}
+
+	//retrieve mapping of each possible device config to factory function to run stereo set with config
+	static const std::map<run_bp_dlls::device_run, RunBpStereoSet_factory> getRunBpFactoryFuncts()
+	{
+		std::map<run_bp_dlls::device_run, RunBpStereoSet_factory> runBpFactoryFuncts;
+		std::map<run_bp_dlls::device_run, std::string> runStereoSetMethodNames = getRunStereoSetMethodNames<beliefPropProcessingDataType>();
+
+		//retrieve the factory functions for each possible device run
+		for (const auto& deviceTypeAndDLL : run_bp_dlls::DLL_FILE_NAMES)
+		{
+			HINSTANCE dll_handle = ::LoadLibrary((s2ws(deviceTypeAndDLL.second)).c_str());
+			if (!dll_handle) {
+				std::cout << "Unable to load DLL!\n";
+				continue;
+			}
+
+			// Get the function from the DLL
+			RunBpStereoSet_factory factory_func = reinterpret_cast<RunBpStereoSet_factory>(
+				::GetProcAddress(dll_handle, (runStereoSetMethodNames[deviceTypeAndDLL.first]).c_str()));
+			if (!factory_func) {
+				std::cout << "Unable to load factory_func from DLL!\n";
+				::FreeLibrary(dll_handle);
+				continue;
+			}
+			runBpFactoryFuncts[deviceTypeAndDLL.first] = factory_func;
+		}
+
+		return runBpFactoryFuncts;
+	}
+};
+
