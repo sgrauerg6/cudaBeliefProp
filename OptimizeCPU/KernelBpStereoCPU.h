40546ad76299947cc9271b53211e1cbb37f978c1
scott gg <sgrauerg@gmail.com>
2021-03-13 23:25:39 -0500

    Bug fix for ARM processing

--- OptimizeCPU/KernelBpStereoCPU.h
+++ OptimizeCPU/KernelBpStereoCPU.h
@@ -169,7 +169,7 @@ public:
 			float16_t* messageLDeviceCurrentCheckerboard0, float16_t* messageRDeviceCurrentCheckerboard0,
 			float16_t* messageUDeviceCurrentCheckerboard1, float16_t* messageDDeviceCurrentCheckerboard1,
 			float16_t* messageLDeviceCurrentCheckerboard1, float16_t* messageRDeviceCurrentCheckerboard1,
-			const float disc_k_bp);
+			const float disc_k_bp, const unsigned int bpSettingsDispVals = 0);
 #endif //COMPILING_FOR_ARM
 
 	template<typename T, typename U, unsigned int DISP_VALS>
