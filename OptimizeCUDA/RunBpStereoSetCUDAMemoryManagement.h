817365d605aaa084fb43691072fe784fc29f6b89
Scott GG <sgrauerg@gmail.com>
2022-06-12 17:06:03 -0400

    Bug fix from removing template from RunBpStereoSetCUDAMemoryManagement and removed from commented out debug statements

--- OptimizeCUDA/RunBpStereoSetCUDAMemoryManagement.h
+++ OptimizeCUDA/RunBpStereoSetCUDAMemoryManagement.h
@@ -9,27 +9,22 @@
 class RunBpStereoSetCUDAMemoryManagement : public RunBpStereoSetMemoryManagement
 {
 public:
-
 	float* allocateDataOnCompDevice(const unsigned int numData) {
-		//std::cout << "ALLOC_GPU\n";
 		//allocate the space for the disparity map estimation
 		float* arrayToAllocate;
 		cudaMalloc((void **) &arrayToAllocate, numData * sizeof(float));
 		return arrayToAllocate;
 	}
 
-	void freeDataOnCompDevice(U arrayToFree) {
-		//std::cout << "FREE_GPU\n";
+	void freeDataOnCompDevice(float* arrayToFree) {
 		cudaFree(arrayToFree);
 	}
 
 	void transferDataFromCompDeviceToHost(float* destArray, const float* inArray, const unsigned int numDataTransfer) {
-		//std::cout << "TRANSFER_GPU\n";
 		cudaMemcpy(destArray, inArray, numDataTransfer * sizeof(float), cudaMemcpyDeviceToHost);
 	}
 
 	void transferDataFromCompHostToDevice(float* destArray, const float* inArray, const unsigned int numDataTransfer) {
-		//std::cout << "TRANSFER_GPU\n";
 		cudaMemcpy(destArray, inArray, numDataTransfer * sizeof(float), cudaMemcpyHostToDevice);
 	}
 };
