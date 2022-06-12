2ecf1a7adfd6df8957fe26442dd47391ac195106
Scott GG <sgrauerg@gmail.com>
2022-06-12 14:06:28 -0400

    Removed unneeded comments

--- BpAndSmoothProcessing/SmoothImage.cpp
+++ BpAndSmoothProcessing/SmoothImage.cpp
@@ -16,8 +16,4 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 */
 
-//Defines the functions used to smooth the input images with a gaussian filter of SIGMA_BP (see bpCudaParameters.cuh) implemented in CUDA
-
 #include "SmoothImage.h"
-
-//functions relating to smoothing the images before running BP
