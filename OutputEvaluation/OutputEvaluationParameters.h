5b8ba66c85aca46fe7d9e5701943362486a741dd
Scott GG <sgrauerg@gmail.com>
2024-01-06 00:38:30 -0500

    Switched const float values to constexpr

--- OutputEvaluation/OutputEvaluationParameters.h
+++ OutputEvaluation/OutputEvaluationParameters.h
@@ -10,28 +10,28 @@
 
 #include <vector>
 
-const float DEFAULT_X_BORDER_GROUND_TRUTH_DISPARITY = 0.0f;
-const float DEFAULT_Y_BORDER_GROUND_TRUTH_DISPARITY = 0.0f;
+constexpr float DEFAULT_X_BORDER_GROUND_TRUTH_DISPARITY = 0.0f;
+constexpr float DEFAULT_Y_BORDER_GROUND_TRUTH_DISPARITY = 0.0f;
 
 //define the difference in disparity for it to be considered a "significant difference"
 //pixels with difference beyond this are called a "bad pixel" if one of the images is the ground truth
-const float SIG_DIFF_THRESHOLD_STEREO_EVAL_THRESHOLD_1 = 0.001f;
-const float SIG_DIFF_THRESHOLD_STEREO_EVAL_THRESHOLD_2 = 2.01f;
-const float SIG_DIFF_THRESHOLD_STEREO_EVAL_THRESHOLD_3 = 5.01f;
-const float SIG_DIFF_THRESHOLD_STEREO_EVAL_THRESHOLD_4 = 10.01f;
+constexpr float SIG_DIFF_THRESHOLD_STEREO_EVAL_THRESHOLD_1 = 0.001f;
+constexpr float SIG_DIFF_THRESHOLD_STEREO_EVAL_THRESHOLD_2 = 2.01f;
+constexpr float SIG_DIFF_THRESHOLD_STEREO_EVAL_THRESHOLD_3 = 5.01f;
+constexpr float SIG_DIFF_THRESHOLD_STEREO_EVAL_THRESHOLD_4 = 10.01f;
 
-const float MAX_DIFF_CAP = SIG_DIFF_THRESHOLD_STEREO_EVAL_THRESHOLD_4;
+constexpr float MAX_DIFF_CAP = SIG_DIFF_THRESHOLD_STEREO_EVAL_THRESHOLD_4;
 
 //don't evaluate the disparities that are within the "border"
 //the x-border is from the left/right sides and the
 //y-border are from the top/bottom
 //initialize to the border of the default ground truth disparity
-const float X_BORDER_SIZE_STEREO_EVAL = DEFAULT_X_BORDER_GROUND_TRUTH_DISPARITY;
-const float Y_BORDER_SIZE_STEREO_EVAL = DEFAULT_Y_BORDER_GROUND_TRUTH_DISPARITY;
+constexpr float X_BORDER_SIZE_STEREO_EVAL = DEFAULT_X_BORDER_GROUND_TRUTH_DISPARITY;
+constexpr float Y_BORDER_SIZE_STEREO_EVAL = DEFAULT_Y_BORDER_GROUND_TRUTH_DISPARITY;
 
 //define a "cap" of the maximum difference between the corresponding disparities
 //(make this infinity if you don't want to cap this value)
-const float MAX_ABS_DIFF_BETWEEN_CORR_DISP = 65504.0f;
+constexpr float MAX_ABS_DIFF_BETWEEN_CORR_DISP = 65504.0f;
 
 template <class T>
 struct OutputEvaluationParameters {
