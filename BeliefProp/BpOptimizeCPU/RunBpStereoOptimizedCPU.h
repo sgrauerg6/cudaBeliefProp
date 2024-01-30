330b09a146e2ab82ae5c5fc7a79d17289d70722b
Scott GG <sgrauerg@gmail.com>
2024-01-30 00:04:51 -0500

    Added comments

--- BeliefProp/BpOptimizeCPU/RunBpStereoOptimizedCPU.h
+++ BeliefProp/BpOptimizeCPU/RunBpStereoOptimizedCPU.h
@@ -33,9 +33,12 @@ template<RunData_t T, unsigned int DISP_VALS, run_environment::AccSetting VECTOR
 inline std::optional<ProcessStereoSetOutput> RunBpStereoOptimizedCPU<T, DISP_VALS, VECTORIZATION>::operator()(const std::array<std::string, 2>& refTestImagePath,
   const beliefprop::BPsettings& algSettings, const beliefprop::ParallelParameters& parallelParams)
 {
+  //set number of threads to use when running code in parallel using OpenMP from input parallel parameters
+  //current setting on CPU is to execute all parallel processing in a run using the same number of parallel threads
   unsigned int nthreads = parallelParams.parallelDimsEachKernel_[beliefprop::BLUR_IMAGES][0][0];
   omp_set_num_threads(nthreads);
 
+  //add settings for current run to output data
   RunData runData;
   runData.addDataWHeader("CURRENT RUN", "OPTIMIZED CPU");
   runData.addDataWHeader("Number of threads", std::to_string(nthreads));
