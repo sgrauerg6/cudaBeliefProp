a98c464deccdc58ba04472cc2f180c5517564090
sgrauerg6 <sgrauerg@gmail.com>
2024-12-09 02:03:52 -0500

    Updated DisparityMap description to state that it is child class of BpImage

--- src/beliefprop/BpImageProcessing/DisparityMap.h
+++ src/beliefprop/BpImageProcessing/DisparityMap.h
@@ -34,7 +34,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 #include "BpResultsEvaluation/DisparityMapEvaluation.h"
 
 /**
- * @brief Class to define disparity map image that is output from bp processing
+ * @brief Child class of BpImage to define disparity map image that is output from bp processing
  * 
  * @tparam T 
  */
