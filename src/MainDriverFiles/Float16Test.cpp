45e9c17827752dbf22a021e456f387838baed5fc
sgrauerg6 <sgrauerg@gmail.com>
2024-12-18 00:52:44 +0000

    Fixes to float16 test

--- src/MainDriverFiles/Float16Test.cpp
+++ src/MainDriverFiles/Float16Test.cpp
@@ -36,24 +36,24 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
  */
 int main(int argc, char** argv)
 {
-//#define TEST_FLOAT61 
+#define TEST_FLOAT61 
 #ifdef TEST_FLOAT61
-  _Float16 x1{1.0};
-  _Float16 x2{3.0};
-  _Float16 x3{5.0};
-  _Float16 x4{7.0};
+  _Float16 x1{1.0f};
+  _Float16 x2{3.0f};
+  _Float16 x3{5.0f};
+  _Float16 x4{7.0f};
 
   _Float16 x_sum = x1 + x2 + x3 + x4;
-  std::cout << "Sum: " << x_sum << std::endl;
+  std::cout << "Sum: " << (float)x_sum << std::endl;
 
   _Float16 x_mul = x1 * x3;
-  std::cout << "Mul: " << x_mul << std::endl;
+  std::cout << "Mul: " << (float)x_mul << std::endl;
 
   _Float16 x_div = x4 / x3;
-  std::cout << "Div: " << x_div << std::endl;
+  std::cout << "Div: " << (float)x_div << std::endl;
 
   _Float16 x_sub = x4 - x2;
-  std::cout << "Sub: " << x_sub << std::endl;
+  std::cout << "Sub: " << (float)x_sub << std::endl;
 #else
   std::cout << "No float16 test" << std::endl;
 #endif //TEST_FLOAT61
