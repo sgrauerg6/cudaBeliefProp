b368f73783d0e9e3e7348e9168fca3deecabc185
sgrauerg6 <sgrauerg@gmail.com>
2024-12-26 22:21:21 -0500

    Formatting adjustments and fixes to run type constraints, specifically regarding support for float16 vectorization in CPU optimization

--- src/RunEval/RunTypeConstraints.h
+++ src/RunEval/RunTypeConstraints.h
@@ -41,7 +41,10 @@ using halftype = __nv_bfloat16;
 using halftype = half;
 #endif //USE_BFLOAT16_FOR_HALF_PRECISION
 template <typename T>
-concept RunData_t = std::is_same_v<T, float> || std::is_same_v<T, double> || std::is_same_v<T, halftype>;
+concept RunData_t = 
+  std::is_same_v<T, float> ||
+  std::is_same_v<T, double> ||
+  std::is_same_v<T, halftype>;
 
 #elif defined(OPTIMIZED_CPU_RUN)
 
@@ -53,19 +56,31 @@ concept RunData_t = std::is_same_v<T, float> || std::is_same_v<T, double> || std
 #include <arm_neon.h>
 //float16_t is used for half data type in ARM processing
 template <typename T>
-concept RunData_t = std::is_same_v<T, float> || std::is_same_v<T, double> || std::is_same_v<T, float16_t>;
+concept RunData_t = 
+  std::is_same_v<T, float> ||
+  std::is_same_v<T, double> ||
+  std::is_same_v<T, float16_t>;
 
 //SIMD types for neon processing on ARM
 template <typename T>
-concept RunDataVect_t = std::is_same_v<T, float64x2_t> || std::is_same_v<T, float32x4_t> || std::is_same_v<T, float16x4_t>;
+concept RunDataVect_t =
+  std::is_same_v<T, float64x2_t> ||
+  std::is_same_v<T, float32x4_t> ||
+  std::is_same_v<T, float16x4_t>;
 
 //data processing on CPU only uses float or double
-//half type gets converted to float for processing and then back to half for storage
+//half type gets converted to float for processing and then back to half for
+//storage
 template <typename T>
-concept RunDataProcess_t = std::is_same_v<T, float> || std::is_same_v<T, double>;
+concept RunDataProcess_t = 
+  std::is_same_v<T, float> ||
+  std::is_same_v<T, double>;
 
 template <typename T>
-concept RunDataVectProcess_t = std::is_same_v<T, float64x2_t> || std::is_same_v<T, float32x4_t>;
+concept RunDataVectProcess_t = 
+  std::is_same_v<T, float64x2_t> ||
+  std::is_same_v<T, float32x4_t>;
+
 #else //COMPILING_FOR_ARM
 
 #ifdef _WIN32
@@ -79,70 +94,138 @@ concept RunDataVectProcess_t = std::is_same_v<T, float64x2_t> || std::is_same_v<
 
 //halftype alias is used for half data type in x86 processing
 template <typename T>
-concept RunData_t = std::is_same_v<T, float> || std::is_same_v<T, double> || std::is_same_v<T, halftype>;
+concept RunData_t =
+  std::is_same_v<T, float> ||
+  std::is_same_v<T, double> ||
+  std::is_same_v<T, halftype>;
 
-//data processing on CPU only uses float or double
-//half type gets converted to float for processing and then back to half for storage
+//data processing on CPU only uses float or double unless half precision
+//vectorization is supported
+//if half precision vectorization not supported, half type gets converted to
+//float for processing and then back to half for storage
+#if defined(HALF_PRECISION_VECTORIZATION)
 template <typename T>
-concept RunDataProcess_t = std::is_same_v<T, float> || std::is_same_v<T, double> || std::is_same_v<T, halftype>;
+concept RunDataProcess_t =
+  std::is_same_v<T, float> ||
+  std::is_same_v<T, double> ||
+  std::is_same_v<T, halftype>;
+#else
+template <typename T>
+concept RunDataProcess_t =
+  std::is_same_v<T, float> ||
+  std::is_same_v<T, double>;
+#endif //HALF_PRECISION_VECTORIZATION
 
 //SIMD types for AVX processing on x86
 #if defined(AVX_512_VECTORIZATION)
 
+//check if half precision vectorization is supported
+#if defined(HALF_PRECISION_VECTORIZATION)
 template <typename T>
-concept RunDataVect_t = std::is_same_v<T, __m256d> || std::is_same_v<T, __m256> || std::is_same_v<T, __m128i> || std::is_same_v<T, __m512d> || std::is_same_v<T, __m512> || std::is_same_v<T, __m256i>;
+concept RunDataVect_t =
+  std::is_same_v<T, __m256d> ||
+  std::is_same_v<T, __m256> ||
+  std::is_same_v<T, __m128i> ||
+  std::is_same_v<T, __m512d> ||
+  std::is_same_v<T, __m512> ||
+  std::is_same_v<T, __m256i> ||
+  std::is_same_v<T, __m512h> ||
+  std::is_same_v<T, __m256h>;
 
 template <typename T>
-concept RunDataVectProcess_t = std::is_same_v<T, __m256d> || std::is_same_v<T, __m256> || std::is_same_v<T, __m512d> || std::is_same_v<T, __m512>;
+concept RunDataVectProcess_t =
+  std::is_same_v<T, __m256d> ||
+  std::is_same_v<T, __m256> ||
+  std::is_same_v<T, __m512d> ||
+  std::is_same_v<T, __m512> ||
+  std::is_same_v<T, __m512h> ||
+  std::is_same_v<T, __m256h>;
+#else
+template <typename T>
+concept RunDataVect_t =
+  std::is_same_v<T, __m256d> ||
+  std::is_same_v<T, __m256> ||
+  std::is_same_v<T, __m128i> ||
+  std::is_same_v<T, __m512d> ||
+  std::is_same_v<T, __m512> ||
+  std::is_same_v<T, __m256i>;
+template <typename T>
+concept RunDataVectProcess_t =
+  std::is_same_v<T, __m256d> ||
+  std::is_same_v<T, __m256> ||
+  std::is_same_v<T, __m512d> ||
+  std::is_same_v<T, __m512>;
+#endif //HALF_PRECISION_VECTORIZATION
 
 #elif defined(AVX_256_VECTORIZATION)
 
+//check if half precision vectorization is supported
+#if defined(HALF_PRECISION_VECTORIZATION)
 template <typename T>
-concept RunDataVect_t = std::is_same_v<T, __m256d> || std::is_same_v<T, __m256> || std::is_same_v<T, __m128i>;
-
-template <typename T>
-concept RunDataVectProcess_t = std::is_same_v<T, __m256d> || std::is_same_v<T, __m256>;
-
-#elif defined(AVX_256_F16_VECTORIZATION)
-
-template <typename T>
-concept RunDataVect_t = std::is_same_v<T, __m256d> || std::is_same_v<T, __m256> || std::is_same_v<T, __m128i> || std::is_same_v<T, __m256h>;
+concept RunDataVect_t =
+  std::is_same_v<T, __m256d> ||
+  std::is_same_v<T, __m256> ||
+  std::is_same_v<T, __m128i> ||
+  std::is_same_v<T, __m256h>;
 
 template <typename T>
-concept RunDataVectProcess_t = std::is_same_v<T, __m256d> || std::is_same_v<T, __m256> || std::is_same_v<T, __m256h>;
-
-#elif defined(AVX_512_F16_VECTORIZATION)
+concept RunDataVectProcess_t =
+  std::is_same_v<T, __m256d> ||
+  std::is_same_v<T, __m256> ||
+  std::is_same_v<T, __m256h>;
+#else
+template <typename T>
+concept RunDataVect_t =
+  std::is_same_v<T, __m256d> ||
+  std::is_same_v<T, __m256> ||
+  std::is_same_v<T, __m128i>;
 
 template <typename T>
-concept RunDataVect_t = std::is_same_v<T, __m256d> || std::is_same_v<T, __m256> || std::is_same_v<T, __m128i> || std::is_same_v<T, __m512d> || std::is_same_v<T, __m512> || std::is_same_v<T, __m256i> || std::is_same_v<T, __m512h> || std::is_same_v<T, __m256h>;
-
-template <typename T>
-concept RunDataVectProcess_t = std::is_same_v<T, __m256d> || std::is_same_v<T, __m256> || std::is_same_v<T, __m512d> || std::is_same_v<T, __m512> || std::is_same_v<T, __m512h> || std::is_same_v<T, __m256h>;
+concept RunDataVectProcess_t =
+  std::is_same_v<T, __m256d> ||
+  std::is_same_v<T, __m256>;
+#endif //HALF_PRECISION_VECTORIZATION
 
 #else
 
-//AVX 512 used by default if AVX vectorization setting not specified
+//AVX 512 without half precision vectorization used by default if AVX
+//vectorization setting not specified
 template <typename T>
-concept RunDataVect_t = std::is_same_v<T, __m256d> || std::is_same_v<T, __m256> || std::is_same_v<T, __m128i> || std::is_same_v<T, __m512d> || std::is_same_v<T, __m512> || std::is_same_v<T, __m256i>;
+concept RunDataVect_t =
+  std::is_same_v<T, __m256d> ||
+  std::is_same_v<T, __m256> ||
+  std::is_same_v<T, __m128i> ||
+  std::is_same_v<T, __m512d> ||
+  std::is_same_v<T, __m512> ||
+  std::is_same_v<T, __m256i>;
 
 template <typename T>
-concept RunDataVectProcess_t = std::is_same_v<T, __m256d> || std::is_same_v<T, __m256> || std::is_same_v<T, __m512d> || std::is_same_v<T, __m512>;
-
+concept RunDataVectProcess_t =
+  std::is_same_v<T, __m256d> ||
+  std::is_same_v<T, __m256> ||
+  std::is_same_v<T, __m512d> ||
+  std::is_same_v<T, __m512>;
 
 #endif //defined(AVX_512_VECTORIZATION)
 #endif //defined(COMPILING_FOR_ARM)
 
 //concepts that allow both single and vectorized types
 template <typename T>
-concept RunDataSingOrVect_t = RunData_t<T> || RunDataVect_t<T>;
+concept RunDataSingOrVect_t =
+  RunData_t<T> ||
+  RunDataVect_t<T>;
 
 template <typename T>
-concept RunDataProcessSingOrVect_t = RunDataProcess_t<T> || RunDataVectProcess_t<T>;
+concept RunDataProcessSingOrVect_t =
+  RunDataProcess_t<T> ||
+  RunDataVectProcess_t<T>;
 
 #endif //defined(OPTIMIZED_CUDA_RUN)
 
 //constraint for pointer to RunData_t
 template <typename T>
-concept RunData_ptr = std::is_pointer_v<T> && RunData_t<std::remove_pointer_t<T>>;
+concept RunData_ptr =
+  std::is_pointer_v<T> &&
+  RunData_t<std::remove_pointer_t<T>>;
 
 #endif //RUN_TYPE_CONSTRAINTS_H_
