c0c072566429760f172190fc4891e0b7fa14bfc4
sgrauerg6 <sgrauerg@gmail.com>
2024-12-27 20:46:22 -0500

    Change to add structured bindings

--- src/RunEval/EvaluateImpResults.cpp
+++ src/RunEval/EvaluateImpResults.cpp
@@ -649,8 +649,10 @@ EvaluateImpResults::GetAvgMedSpeedupOverBaselineSubsets(
     GetBaselineRuntimeData(
       baseline_runtimes_path_desc,
       run_eval::kOptimizedRuntimeHeader);
-  auto& baseline_name = baseline_runtime_data.first;
-  auto& baseline_runtimes_to_sig = baseline_runtime_data.second;
+  const auto& [baseline_name, baseline_runtimes_to_sig] =
+    baseline_runtime_data;
+  //auto& baseline_name = baseline_runtime_data.first;
+  //auto& baseline_runtimes_to_sig = baseline_runtime_data.second;
   //TODO: figure out how below gets error
   /*const auto [baseline_name, baseline_runtimes_to_sig] =
     GetBaselineRuntimeData(
