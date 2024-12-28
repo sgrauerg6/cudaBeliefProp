78256946d61ac55ae8c477754be2e64ccb95b3cf
sgrauerg6 <sgrauerg@gmail.com>
2024-12-28 15:37:53 -0500

    Moved header file from .cpp to .h file since needed there

--- src/RunEval/EvaluateAcrossRuns.cpp
+++ src/RunEval/EvaluateAcrossRuns.cpp
@@ -33,7 +33,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 #include <algorithm>
 #include "RunSettingsParams/InputSignature.h"
 #include "EvaluateAcrossRuns.h"
-#include "RunResultsSpeedups.h"
 
 /**
   * @brief Generate data for evaluating results across runs
--- src/RunEval/EvaluateAcrossRuns.h
+++ src/RunEval/EvaluateAcrossRuns.h
@@ -33,6 +33,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 #include <vector>
 #include <filesystem>
 #include "RunEvalConstsEnums.h"
+#include "RunResultsSpeedups.h"
 
 /**
  * @brief Structure to store data with mappings for evaluating results across
