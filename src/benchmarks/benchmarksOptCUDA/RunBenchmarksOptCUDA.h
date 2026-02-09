4bea2a16fb06f8e7312e4d2d750fff3a7e17f4ba
sgrauerg6 <sgrauerg@gmail.com>
2026-02-09 18:17:05 -0500

    Changed copyright year to current year

--- src/benchmarks/benchmarksOptCUDA/RunBenchmarksOptCUDA.h
+++ src/benchmarks/benchmarksOptCUDA/RunBenchmarksOptCUDA.h
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2024 Scott Grauer-Gray
+Copyright (C) 2026 Scott Grauer-Gray
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
